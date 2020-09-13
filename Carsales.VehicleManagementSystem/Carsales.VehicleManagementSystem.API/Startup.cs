4db3fc2d380a752eed93f9869502b1ae2e34060e
archerjessie <archerjessieliu@gmail.com>
2020-09-13 14:06:58 +1000

    update .net core version

--- Carsales.VehicleManagementSystem/Carsales.VehicleManagementSystem.API/Startup.cs
+++ Carsales.VehicleManagementSystem/Carsales.VehicleManagementSystem.API/Startup.cs
@@ -26,7 +26,7 @@ namespace Carsales.VehicleManagementSystem.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddSingleton<IVehicleRepository>(new VehicleRepository());
             services.AddScoped<IVehicleService>(provider => new VehicleService(provider.GetService<IVehicleRepository>()));
