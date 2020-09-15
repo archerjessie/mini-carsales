c680281988ad2d8120507feb70d8d6a9bf1a0c86
archerjessie <66236559+archerjessie@users.noreply.github.com>
2020-09-15 11:17:35 +1000

    api methods

---
+++ carsales-vms-frontend/src/api/vehicleApi.js
@@ -0,0 +1,24 @@
+import { handleResponse, handleError } from './apiUtils'
+const baseUrl = process.env.REACT_APP_API_URL + '/api/vehicle/'
+
+export function getVehicle() {
+  return fetch(baseUrl).then(handleResponse).catch(handleError)
+}
+
+export function createVehicle(vehicleType, vehicle) {
+  console.log(
+    JSON.stringify({
+      ...vehicle,
+    }),
+  )
+
+  return fetch(baseUrl + vehicleType, {
+    method: 'post',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({
+      ...vehicle,
+    }),
+  })
+    .then(handleResponse)
+    .catch(handleError)
+}
